f05b89a7ace1b1d8f13b88172b47593fc406be95
Noyyu <Nikki.Norberg@hotmail.com>
2025-11-21 11:45:39 +0100

    Added customer and Admin menu

--- The_Invincible_Bank/Bank.cs
+++ The_Invincible_Bank/Bank.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Security.Principal;
@@ -11,24 +12,18 @@ namespace The_Invincible_Bank
 {
     internal class Bank
     {
-        private List<Admin> adminAccounts;
-        private List<Customer> customerAccounts;
-
-        private Admin adminOne;
-        private User userOne;
+        private List<User> userAccounts;
 
         private int currentUserAccount = -1;
         
         public Bank()
         {
             var adminOne = new Admin(1111, "1111");
-            var userOne = new Customer(2222, "2222");
+            var customerOne = new Customer(2222, "2222");
+            userAccounts = new List<User>();
 
-            adminAccounts = new List<Admin>();
-            customerAccounts = new List<Customer>();
-
-            adminAccounts.Add(adminOne);
-            customerAccounts.Add(userOne);
+            userAccounts.Add(adminOne);
+            userAccounts.Add(customerOne);
         }
 
         public bool Transfer(int accountOne, int accountTwo, decimal sum)
@@ -45,33 +40,31 @@ namespace The_Invincible_Bank
 
         private bool CheckSenderAccountValidity(int accountOne, decimal sum)
         {
-            
-            foreach (BankAccount account in customerAccounts[currentUserAccount].Accounts) //Checks if the stated account is owned by the user
+            if (userAccounts[currentUserAccount] is Customer customer)
             {
-                if (account.AccountNumber == accountOne)
+                foreach (var account in customer.Accounts) //Gets the worth of all the accounts of the current user
                 {
-                    if (account.Sum >= sum) //If theres enough money on the account
+                    if (account.AccountNumber == accountOne)
                     {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
+                        if (account.Sum >= sum) //If theres enough money on the account
+                        {
+                            return true;
+                        }
                     }
                 }
             }
             return false; //Retunerar false ifall kontot antingen inte finns eller inte har tillräckligt mycket pengar.
         }
+
         private bool CheckReceaverAccountValidity(int accountTwo)
         {
             int counter = 0;
-            foreach (Customer accountA in customerAccounts) // Steps in to the list of accounts
+            foreach (Customer accountA in userAccounts) // Steps in to the list of accounts
             {
-                foreach (BankAccount accountB in customerAccounts[counter].Accounts) //Steps in to the list of accounts the user own
+                foreach (BankAccount accountB in accountA.Accounts) //Steps in to the list of accounts the user own
                 {
                     if (accountB.AccountNumber == accountTwo)
                     {
-
                         return true;
                     }
                 }
@@ -94,10 +87,15 @@ namespace The_Invincible_Bank
         private bool CheckAccountBorrowValidity(decimal sum)
         {
             decimal totalWorth = 0;
-            foreach (BankAccount account in customerAccounts[currentUserAccount].Accounts) //Gets the worth of all the accounts of the current user
+
+            if (userAccounts[currentUserAccount] is Customer customer)
             {
-                totalWorth += account.Sum;
+                foreach (var account in customer.Accounts) //Gets the worth of all the accounts of the current user
+                {
+                    totalWorth += account.Sum;
+                }
             }
+
             if (totalWorth * 5 > sum) //Checks if the the worth is more or less than the borrow amount
             {
                 return true;
@@ -120,11 +118,74 @@ namespace The_Invincible_Bank
             password = Console.ReadLine();
 
             Customer newAccount = new Customer(securityNumber, password);
-            customerAccounts.Add(new Customer(securityNumber, password));
+            userAccounts.Add(new Customer(securityNumber, password));
 
             UI.DisplayMessage("Account was created");
         }
 
+        private void adminMenu()
+        {
+            var admin = userAccounts[currentUserAccount] as Admin;
+            bool exist = false;
+
+            while (!exist)
+            {
+                UI.DisplayMessage("1: Create new user\n2: Update currency value\n3: Log out");
+
+                switch (Input.GetNumberFromUser(1,3))
+                {
+                    case 1:
+                        CreateNewUser();
+                        break;
+                    case 2:
+                        //Currency.ShowCurrencyValues()
+                        //Currency.UpdateCurrency();
+                        //Currency.ShowCurrencyValues()
+                        break;
+                    case 3:
+                        exist = true;
+                        break;
+                }
+            }
+
+        }
+        private void customerMenu()
+        {
+            var customer = userAccounts[currentUserAccount] as Customer;
+            bool exist = false;
+
+            while (!exist)
+            {
+                UI.DisplayMessage("1: Show Accounts\n2: Cretae new account\n3: Transfer money \n4: Convert account currency\n5: Show account history\n6: Borrow money\n7: Log out");
+
+                switch (Input.GetNumberFromUser(1, 7))
+                {
+                    case 1:
+                        customer.ShowAccounts();
+
+                        break;
+                    case 2:
+                        //customer.CreateBankAccount(Input.GetString(), Input.GetCurrency());
+                        break;
+                    case 3:
+                        //customer.Transfer();
+                        break;
+                    case 4:
+                        //customer.ConvertAccountCurrency(Input.getBankAccount(), Input.getCurrency())
+                        break;
+                    case 5:
+                        customer.ShowAccounts();
+                        //customer.ShowAccountHistory(Input.getBankAccount());
+                        break;
+                    case 6:
+                        //Borrow(Input.getBankAccount(), Input.GetDecimalFromUser());
+                        break;
+                    case 7:
+                        exist = true;
+                        break;
+                }
+            }
+        }
         private int UserLogIn() //If this returns -1, the user failed to log in within 3 tries
         {
 
@@ -134,6 +195,13 @@ namespace The_Invincible_Bank
             int userIndex = 0;
             int userLoginTries = 0;
 
+            UI.DisplayMessage("1: Log in\n2: Exit program");
+            if (Input.GetNumberFromUser(1,2) == 2)
+            {
+                return -1;
+            }
+            Console.Clear();
+
             UI.DisplayMessage("Security number: ");
             while (!int.TryParse(Console.ReadLine(), out inputSecurityNumber) && inputSecurityNumber.ToString().Length != 4)
             {
@@ -141,7 +209,7 @@ namespace The_Invincible_Bank
             }
 
             //check if account exists in the user account list
-            foreach (var user in customerAccounts)
+            foreach (var user in userAccounts)
             {               
                 if (user.SecurityNumber == inputSecurityNumber) //If we found the security number in the list of users
                 {                   
@@ -167,6 +235,7 @@ namespace The_Invincible_Bank
                         }
                     }
                     Console.WriteLine("Welcome!"); //Replace
+                    userIndex = userAccounts.IndexOf(user);
                     accountExists = true;
                     break;                                   
                 }
@@ -175,7 +244,7 @@ namespace The_Invincible_Bank
             {
                 UI.DisplayMessage("This account does not exist in our bank.\nCreate a new account or try again?\nNew account: 1 | Try again: 2");
 
-                if (Input.GetNumberFromUser(1, 2) == 1)
+                if (Input.GetNumberFromUser(1, 2) == 2)
                 {
                     UserLogIn();
                 }
@@ -191,15 +260,27 @@ namespace The_Invincible_Bank
         {
             //Koden börjar och slutar här.
             UI.DisplayLoggo();
-            // Menu
+            bool exist = false;
 
-            currentUserAccount =  UserLogIn(); //Logs in to user and sets the current user index
-            if (currentUserAccount == -1) //The user failed to login within 3 tries. 
+            while (!exist)
             {
-                return false;
+                currentUserAccount = UserLogIn(); //Logs in to user and sets the current user index
+                if (currentUserAccount == -1) //The user failed to login within 3 tries. 
+                {
+                    return false;
+                }
+                if (userAccounts[currentUserAccount] is Customer)
+                {
+                    customerMenu();
+                    currentUserAccount = -2;
+                }
+                else if (userAccounts[currentUserAccount] is Admin)
+                {
+                    adminMenu();
+                    currentUserAccount = -2;
+                }
             }
 
-            UI.DisplayMessage("1: Show Accounts\n2: Cretae new account\n3: Transfer money \n4: Convert account currency\n5: Show account history\n6: Borrow money");
 
             return true;
         }
